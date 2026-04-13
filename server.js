892be638c844bd149977a4b615e185a12c68b5d1
guttihomika <guttihomika1@gmail.com>
2026-04-13 19:46:05 +0530

    Implement number guessing game API

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+
+app.post('/guess', (req, res) => {
+    const userGuess = req.body.guess;
+
+    if (userGuess === secretNumber) {
+        res.json({ result: "Correct! You won!" });
+        secretNumber = Math.floor(Math.random() * 100) + 1; // Reset game
+    } else if (userGuess > secretNumber) {
+        res.json({ result: "Too high!" });
+    } else {
+        res.json({ result: "Too low!" });
+    }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
