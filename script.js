2cc59a213c4c736dfbd928df7f6e7278df97a72f
guttihomika <guttihomika1@gmail.com>
2026-04-13 19:45:35 +0530

    Add async makeGuess function for user input

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+async function makeGuess() {
+    const guess = document.getElementById('guessInput').value;
+    const message = document.getElementById('message');
 
+    const response = await fetch('http://localhost:3000/guess', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ guess: parseInt(guess) })
+    });
+
+    const data = await response.json();
+    message.innerText = data.result;
+}
